068b625418ce0342d33318f5f1b324d51df123ce
Tomek Karwowski <to.karwowski@gmail.com>
2021-01-03 21:27:20 +0100

    set and rm works

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,38 +1,32 @@
 use clap::{load_yaml, App, ArgMatches};
-use std::error::Error;
-use std::process;
 
-fn main() {
+use kvs::Result;
+
+fn main() -> Result<()> {
     let yaml = load_yaml!("cli.yaml");
     let matches = App::from(yaml).get_matches();
 
-    if let Err(e) = run(&matches) {
-        eprintln!("Application error: {}", e);
-        process::exit(1);
-    }
-
-    eprintln!("unimplemented");
-    process::exit(1);
+    run(&matches)
 }
 
-pub fn run(matches: &ArgMatches) -> Result<(), Box<dyn Error>> {
-    let mut store = kvs::KvStore::new();
+pub fn run(matches: &ArgMatches) -> Result<()> {
+    let mut store = kvs::KvStore::open("kvs.db")?;
 
     match matches.subcommand() {
         Some(("set", set_matches)) => {
-            let key = set_matches.value_of("key").unwrap();
-            let value = set_matches.value_of("value").unwrap();
-            store.set(key, value);
+            let key = set_matches.value_of("key").unwrap().to_owned();
+            let value = set_matches.value_of("value").unwrap().to_owned();
+            store.set(key, value)?;
         }
         Some(("get", get_matches)) => {
-            let key = get_matches.value_of("key").unwrap();
-            store.get(key);
+            let key = get_matches.value_of("key").unwrap().to_owned();
+            store.get(key)?;
         }
         Some(("rm", remove_matches)) => {
-            let key = remove_matches.value_of("key").unwrap();
-            store.remove(key);
+            let key = remove_matches.value_of("key").unwrap().to_owned();
+            store.remove(key)?;
         }
-        _ => panic!(),
+        _ => unreachable!(),
     }
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,51 +1,73 @@
-//! # KvStore, a Key-Value Store
+//! # KvStore, a CLI Key-Value Store
 //!
-//! Implementation of redis-like key-value store
+//! Implementation of command-line redis-like key-value store
 //! as part of [PingCAP Talent Plan](https://github.com/pingcap/talent-plan).
 //!
 //! Nothing fancy, but should allow you to [KvStore::set], [KvStore::get] and [KvStore::remove]
 //! in a in-memory cache.
 #![warn(missing_docs)]
-use std::collections::HashMap;
-use std::hash::Hash;
+
+use std::path::PathBuf;
+use std::error::Error;
+use std::result;
+use std::fs::{File, OpenOptions};
+use std::io::Write;
+use serde::{Serialize, Deserialize};
+
+
+// TODO(tkarwwoski): might use https://docs.rs/fehler/1.0.0/fehler/ instead
+pub type Result<T> = result::Result<T, Box<dyn Error>>;
+
+#[derive(Serialize, Deserialize, Debug)]
+enum Command {
+    Set((String, String)),
+    Remove(String),
+}
 
 /// KvStore is a key-value store allowing you store values in-memory with O(1) lookup time.
 /// # Example
 /// ```
 /// use kvs::KvStore;
-/// let mut store = KvStore::new();
+/// let mut store = KvStore::open();
 ///
-/// store.set("key1", "value1");
-/// assert_eq!(Some("value1"), store.get("key1"));
+/// store.set("key1".to_owned(), "value1".to_owned());
+/// assert_eq!(Some("value1".to_owned()), store.get("key1".to_owned()));
 ///
-/// store.remove("key1");
-/// assert_eq!(None, store.get("key1"));
+/// store.remove("key1".to_owned());
+/// assert_eq!(None, store.get("key1".to_owned()));
 /// ```
 #[derive(Debug)]
-pub struct KvStore<K, V> {
-    store: HashMap<K, V>,
+pub struct KvStore {
+    log: File,
 }
 
-impl<K: Eq + Hash, V: Clone> KvStore<K, V> {
-    /// Initialize new store
-    pub fn new() -> Self {
-        KvStore {
-            store: HashMap::new(),
-        }
+impl KvStore {
+    pub fn open(path: impl Into<PathBuf>) -> Result<Self> {
+        let mut file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .append(true)
+            .open(path.into())
+            .unwrap();
+        Ok(KvStore { log: file })
     }
 
     /// Set a value. Overrides the value if key is already present
-    pub fn set(&mut self, key: K, value: V) {
-        self.store.insert(key, value);
+    pub fn set(&mut self, key: String, value: String) -> Result<()> {
+        let command = serde_json::to_string(&Command::Set((key, value))).unwrap();
+        self.log.write((command + "\n").as_bytes())?;
+        Ok(())
     }
 
     /// Get a value.
-    pub fn get(&self, key: K) -> Option<V> {
-        self.store.get(&key).map(|value| value.clone())
+    pub fn get(&self, key: String) -> Result<Option<String>> {
+        unimplemented!()
     }
 
     /// Remove a value. If value wasn't present, nothing happens.
-    pub fn remove(&mut self, key: K) {
-        self.store.remove(&key);
+    pub fn remove(&mut self, key: String) -> Result<()> {
+        let command = serde_json::to_string(&Command::Remove(key)).unwrap();
+        self.log.write((command + "\n").as_bytes())?;
+        Ok(())
     }
 }
